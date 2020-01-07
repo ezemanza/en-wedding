e87efd3700721703ae68ee105dbbc0b424b4c85d
Ezequiel Manzano <ezequiel.manzano@gmail.com>
2020-01-07 00:29:11 -0300

    Attemp to fix prod

--- assets/js/wedding/script.js
+++ assets/js/wedding/script.js
@@ -332,13 +332,13 @@ $(document).ready(function () {
 
 	// 07. COUNTDOWN
 	//===================================================================================
-	var userTZ = moment.tz.guess(true);
-	var theday = moment.tz("2020-03-28 19:00", "America/Argentina/Buenos_Aires");
-	var theDayUserTz = moment(theday).tz(userTZ).toDate();
-	$('#countdown').countdown({ until: theDayUserTz, format: 'DHMS' });
-	$('#countdown').countdown($.countdown.regionalOptions['custom-label']);
+	// var userTZ = moment.tz.guess(true);
+	// var theday = moment.tz("2020-03-28 19:00", "America/Argentina/Buenos_Aires");
+	// var theDayUserTz = moment(theday).tz(userTZ).toDate();
+	// $('#countdown').countdown({ until: theDayUserTz, format: 'DHMS' });
+	// $('#countdown').countdown($.countdown.regionalOptions['custom-label']);
 
-	$('#date-countdown').countdown({ until: theDayUserTz, format: 'DHMS' });
+	// $('#date-countdown').countdown({ until: theDayUserTz, format: 'DHMS' });
 
 	// 08. MOBILE MENU
 	//==================================================================================
